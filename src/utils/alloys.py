82b0b7c2b3c84a1d831309633dd4870c61732563
Dedliiih <benjafu78@gmail.com>
2024-11-17 23:22:03 -0300

    COMMIT-001 Primer commit, agregando primeras funcionalidades de la calculadora

---
+++ src/utils/alloys.py
@@ -0,0 +1,11 @@
+components = [
+    "Aluminio (Al)", "Hierro (Fe)", "Níquel (Ni)", "Cobre (Cu)", "Zinc (Zn)",
+    "Titanio (Ti)", "Magnesio (Mg)", "Cromo (Cr)", "Molibdeno (Mo)",
+    "Vanadio (V)", "Tungsteno (W)", "Manganeso (Mn)", "Silicio (Si)",
+    "Estaño (Sn)", "Carbono (C)", "Fósforo (P)", "Azufre (S)", "Boro (B)",
+    "Cobalto (Co)", "Niobio (Nb)", "Tantalio (Ta)", "Zirconio (Zr)", "Oro (Au)",
+    "Plata (Ag)", "Platino (Pt)", "Paladio (Pd)", "Rodio (Rh)", "Neodimio (Nd)",
+    "Cerio (Ce)", "Lantano (La)", "Praseodimio (Pr)", "Samario (Sm)",
+    "Litio (Li)", "Cadmio (Cd)", "Antimonio (Sb)", "Bismuto (Bi)", "Mercurio (Hg)",
+    "Berilio (Be)", "Hafnio (Hf)", "Telurio (Te)"
+]
